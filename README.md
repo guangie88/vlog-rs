438a527363e53d8bb6f05996858deb75ec566a5f
Weiguang <chen.weiguang@gmail.com>
2018-04-14 22:53:13 +0800

    Initial implementation of macros (#1)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vlog-rs
-Macros to do stdout / stderr logs based on verbosity level
+
+Macros to do stdout / stderr logs based on verbosity level.
+
+[![Build Status](https://travis-ci.org/guangie88/vlog-rs.svg?branch=master)](https://travis-ci.org/guangie88/vlog-rs)
+[![codecov](https://codecov.io/gh/guangie88/vlog-rs/branch/master/graph/badge.svg)](https://codecov.io/gh/guangie88/vlog-rs)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ examples/simple.rs
@@ -0,0 +1,103 @@
+#[macro_use]
+extern crate vlog;
+
+use vlog::set_verbosity_level;
+
+fn print_v0() {
+    set_verbosity_level(0);
+    println!("\nSet verbosity level to 0!");
+
+    v0!("[STDOUT] v0");
+    v0!("[STDOUT] {}", "formatted v0");
+    v1!("[STDOUT] v1 - MUST NOT appear");
+    v1!("[STDOUT] {}", "formatted v1 - MUST NOT appear");
+    v2!("[STDOUT] v2 - MUST NOT appear");
+    v2!("[STDOUT] {}", "formatted v2 - MUST NOT appear");
+    v3!("[STDOUT] v3 - MUST NOT appear");
+    v3!("[STDOUT] {}", "formatted v3 - MUST NOT appear");
+
+    ve0!("[STDERR] v0");
+    ve0!("[STDERR] {}", "formatted v0");
+    ve1!("[STDERR] v1 - MUST NOT appear");
+    ve1!("[STDERR] {}", "formatted v1 - MUST NOT appear");
+    ve2!("[STDERR] v2 - MUST NOT appear");
+    ve2!("[STDERR] {}", "formatted v2 - MUST NOT appear");
+    ve3!("[STDERR] v3 - MUST NOT appear");
+    ve3!("[STDERR] {}", "formatted v3 - MUST NOT appear");
+}
+
+fn print_v1() {
+    set_verbosity_level(1);
+    println!("\nSet verbosity level to 1!");
+
+    v0!("[STDOUT] v0");
+    v0!("[STDOUT] {}", "formatted v0");
+    v1!("[STDOUT] v1");
+    v1!("[STDOUT] {}", "formatted v1");
+    v2!("[STDOUT] v2 - MUST NOT appear");
+    v2!("[STDOUT] {}", "formatted v2 - MUST NOT appear");
+    v3!("[STDOUT] v3 - MUST NOT appear");
+    v3!("[STDOUT] {}", "formatted v3 - MUST NOT appear");
+
+    ve0!("[STDERR] v0");
+    ve0!("[STDERR] {}", "formatted v0");
+    ve1!("[STDERR] v1");
+    ve1!("[STDERR] {}", "formatted v1");
+    ve2!("[STDERR] v2 - MUST NOT appear");
+    ve2!("[STDERR] {}", "formatted v2 - MUST NOT appear");
+    ve3!("[STDERR] v3 - MUST NOT appear");
+    ve3!("[STDERR] {}", "formatted v3 - MUST NOT appear");
+}
+
+fn print_v2() {
+    set_verbosity_level(2);
+    println!("\nSet verbosity level to 2!");
+
+    v0!("[STDOUT] v0");
+    v0!("[STDOUT] {}", "formatted v0");
+    v1!("[STDOUT] v1");
+    v1!("[STDOUT] {}", "formatted v1");
+    v2!("[STDOUT] v2");
+    v2!("[STDOUT] {}", "formatted v2");
+    v3!("[STDOUT] v3 - MUST NOT appear");
+    v3!("[STDOUT] {}", "formatted v3 - MUST NOT appear");
+
+    ve0!("[STDERR] v0");
+    ve0!("[STDERR] {}", "formatted v0");
+    ve1!("[STDERR] v1");
+    ve1!("[STDERR] {}", "formatted v1");
+    ve2!("[STDERR] v2");
+    ve2!("[STDERR] {}", "formatted v2");
+    ve3!("[STDERR] v3 - MUST NOT appear");
+    ve3!("[STDERR] {}", "formatted v3 - MUST NOT appear");
+}
+
+fn print_v3() {
+    set_verbosity_level(3);
+    println!("\nSet verbosity level to 3!");
+
+    v0!("[STDOUT] v0");
+    v0!("[STDOUT] {}", "formatted v0");
+    v1!("[STDOUT] v1");
+    v1!("[STDOUT] {}", "formatted v1");
+    v2!("[STDOUT] v2");
+    v2!("[STDOUT] {}", "formatted v2");
+    v3!("[STDOUT] v3");
+    v3!("[STDOUT] {}", "formatted v3");
+
+    ve0!("[STDERR] v0");
+    ve0!("[STDERR] {}", "formatted v0");
+    ve1!("[STDERR] v1");
+    ve1!("[STDERR] {}", "formatted v1");
+    ve2!("[STDERR] v2");
+    ve2!("[STDERR] {}", "formatted v2");
+    ve3!("[STDERR] v3");
+    ve3!("[STDERR] {}", "formatted v3");
+}
+
+fn main() {
+    print_v0();
+    print_v1();
+    print_v2();
+    print_v3();
+}
