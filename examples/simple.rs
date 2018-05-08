5a388164fc3a807121f79dab249e9f786894c2b1
Weiguang <chen.weiguang@gmail.com>
2018-05-08 21:24:36 +0800

    Fix module namespacing issue (#9)
    * Fix macro calls, split macros and tests
    
    * Fix test
    
    * Improve README with ve

--- examples/simple.rs
+++ examples/simple.rs
@@ -95,9 +95,37 @@ fn print_v3() {
     ve3!("[STDERR] {}", "formatted v3");
 }
 
+mod nest {
+    use super::*;
+
+    pub fn print_another_v3() {
+        set_verbosity_level(3);
+        println!("\nAnother round of verbosity level 3!");
+
+        v0!("[STDOUT] v0");
+        v0!("[STDOUT] {}", "formatted v0");
+        v1!("[STDOUT] v1");
+        v1!("[STDOUT] {}", "formatted v1");
+        v2!("[STDOUT] v2");
+        v2!("[STDOUT] {}", "formatted v2");
+        v3!("[STDOUT] v3");
+        v3!("[STDOUT] {}", "formatted v3");
+
+        ve0!("[STDERR] v0");
+        ve0!("[STDERR] {}", "formatted v0");
+        ve1!("[STDERR] v1");
+        ve1!("[STDERR] {}", "formatted v1");
+        ve2!("[STDERR] v2");
+        ve2!("[STDERR] {}", "formatted v2");
+        ve3!("[STDERR] v3");
+        ve3!("[STDERR] {}", "formatted v3");
+    }
+}
+
 fn main() {
     print_v0();
     print_v1();
     print_v2();
     print_v3();
+    nest::print_another_v3();
 }
